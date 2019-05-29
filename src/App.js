f165e74391e0f4d9de6d3850e998bdc73f0403e3
Zolton <gmzesq@gmail.com>
2019-05-29 17:40:29 -0400

    After 3 hours of self-study, finally learned what I lecture was supposed to teach.  Starting today's project

--- src/App.js
+++ src/App.js
@@ -3,10 +3,21 @@ import React from 'react';
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  // this component is going to take care of state, 
+  //and any change handlers you need to work with your state
+    constructor () {
+      super ();
+      this.state = {
+
+      }
+    }
   render() {
     return (
       <div>
+        <TodoList />
+        <Todo />
+        <TodoForm />
+        
         <h2>Welcome to your Todo App!</h2>
       </div>
     );
