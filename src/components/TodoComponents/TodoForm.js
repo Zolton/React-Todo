afe89b313943ea41bef703b4782bd5346637def9
Zolton <gmzesq@gmail.com>
2019-05-29 23:50:38 -0400

    MVP

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+function TodoForm (props) {
+    return (
+        <form className = "form" onSubmit={e=> props.addTask(e)}>
+        <input 
+        type="text" 
+        name="taskInput"
+        placeholder="Enter task here" 
+        value={props.taskInput}
+        onChange={e=>props.changeHandler(e)}
+        />
+         <button>Submit</button>
+         <button>Clear</button>
+         </form>
+    )
+}
+
+export default TodoForm
